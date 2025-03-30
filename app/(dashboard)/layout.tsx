2d6052ed31f83437d4be64c835cd1c0501e3ce5c
Zach Doster <zach@joinhallway.com>
2025-03-29 23:27:31 -0700

    remove max width

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -82,7 +82,7 @@ function UserMenu() {
 function Header() {
 	return (
 		<header className="border-b border-gray-200">
-			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+			<div className="mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
 				<Link href="/" className="flex items-center">
 					<Globe2 className="h-6 w-6 text-red-500" />
 					<span className="ml-2 text-xl font-semibold text-gray-900">
