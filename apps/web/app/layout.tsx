4f78b179af1f360a75b5ea6e2c83e7360eef795c
Zach Doster <zach@joinhallway.com>
2025-06-26 02:37:54 -0500

    add gtag for adword

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { UserProvider } from "@/lib/auth";
 import { getUser } from "@/lib/db/queries";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 import { Manrope } from "next/font/google";
 import "./globals.css";
@@ -31,6 +32,19 @@ export default function RootLayout({
 			lang="en"
 			className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
 		>
+			{/* Google tag (gtag.js) */}
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=AW-17267419988"
+			/>
+			<Script id="google-analytics">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'AW-17267419988');
+				`}
+			</Script>
 			<body className="min-h-[100dvh] bg-gray-50">
 				<UserProvider userPromise={userPromise}>{children}</UserProvider>
 				<Toaster richColors />
